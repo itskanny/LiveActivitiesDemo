7c06b137c1ff98eb40f84a3dca8ec2a25cca8de3
muhammad-umair02-confiz <112553211+muhammad-umair02-confiz@users.noreply.github.com>
2022-12-03 02:02:44 +0500

    Initial Commit

---
+++ FoodDeliverTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FoodDeliverTest
+//
+//  Created by Muhammad Umair on 03/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodDeliverTest/FoodDeliverTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliverTestApp.swift
+//  FoodDeliverTest
+//
+//  Created by Muhammad Umair on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliverTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
